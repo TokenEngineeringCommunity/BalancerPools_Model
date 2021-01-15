5a86aa1d1a407de57680e43ca4c60c1da664a7c0
Raul <raul.mf86@gmail.com>
2021-01-15 08:16:08 +0100

    WIP

---
+++ model/parts/system.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_json('model/parts/actions-WETH-DAI-0x8b6e6e7b5b3801fed2cafd4b22b8a16c2f2db21a.json')
+
+
+def p_action_decoder(params, step, history, current_state):
+    '''
+    In this simplified model of Balancer, we have not modeled user behavior. Instead, we map events to actions.
+    '''
+    prev_timestep = current_state['timestep']
+    print(prev_timestep)
+    if step > 1:
+        prev_timestep -= 1
+
+    # skip the first event, as they are already accounted for in the initial conditions of the system
+    data_counter = prev_timestep + 1
+    action = df['action'][data_counter]
+    return action
+
+def su_update_pool(params, step, history, current_state, input_):
---
+++ tests/test_sytem.py
@@ -0,0 +1,23 @@
+import unittest
+from decimal import Decimal
+
+from model.balancer_math import BalancerMath
+from model.parts.system import p_action_decoder
+
+
+class TestSystem(unittest.TestCase):
+
+    def test_p_action_decoder(self):
+        action = p_action_decoder({}, 0 , {}, { 'timestep': 0})
+        self.assertDictEqual(action, {
+            "type": "swap",
+            "token_in": "DAI",
+            "token_amount_in": "11861.328308360999600128",
+            "token_out": "WETH",
+            "token_amount_out": "20.021734699893455844"
+        })
+
+
+
+if __name__ == '__main__':
+    unittest.main()
