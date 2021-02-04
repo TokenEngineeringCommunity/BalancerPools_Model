722a1282967685f0769f8cc24ea07d3be6bf6ab2
Andrew Chiw <randomshinichi4869@gmail.com>
2021-02-04 10:18:21 +0100

    moved things into produce_actions() so Raul can have his own section

--- data/pulldata.py
+++ data/pulldata.py
@@ -43,61 +43,64 @@ def query_save(client, pool_address: str, event_type: str, sql: str):
     df = query(client, sql)
     save_queries(pool_address, event_type, df)
 
-if not os.path.exists(args.pool_address):
-    new_sql = 'select * from blockchain-etl.ethereum_balancer.BFactory_event_LOG_NEW_POOL where pool="{}"'.format(args.pool_address)
-    swap_sql = 'select * from blockchain-etl.ethereum_balancer.BPool_event_LOG_SWAP where contract_address="{}" order by block_number'.format(args.pool_address)
-    join_sql = 'select * from blockchain-etl.ethereum_balancer.BPool_event_LOG_JOIN where contract_address="{}" order by block_number'.format(args.pool_address)
-    exit_sql = 'select * from blockchain-etl.ethereum_balancer.BPool_event_LOG_EXIT where contract_address="{}" order by block_number'.format(args.pool_address)
-    transfer_sql = 'select * from blockchain-etl.ethereum_balancer.BPool_event_Transfer where contract_address="{}" order by block_number'.format(args.pool_address)
-    with open("view_pools_fees.sql", "r") as f:
-        fees_sql = f.read().format(args.pool_address)
-    with open("view_pools_tokens_denorm_weights.sql", "r") as f:
-        denorms_sql = f.read().format(args.pool_address)
+def produce_actions():
+    if not os.path.exists(args.pool_address):
+        new_sql = 'select * from blockchain-etl.ethereum_balancer.BFactory_event_LOG_NEW_POOL where pool="{}"'.format(args.pool_address)
+        swap_sql = 'select * from blockchain-etl.ethereum_balancer.BPool_event_LOG_SWAP where contract_address="{}" order by block_number'.format(args.pool_address)
+        join_sql = 'select * from blockchain-etl.ethereum_balancer.BPool_event_LOG_JOIN where contract_address="{}" order by block_number'.format(args.pool_address)
+        exit_sql = 'select * from blockchain-etl.ethereum_balancer.BPool_event_LOG_EXIT where contract_address="{}" order by block_number'.format(args.pool_address)
+        transfer_sql = 'select * from blockchain-etl.ethereum_balancer.BPool_event_Transfer where contract_address="{}" order by block_number'.format(args.pool_address)
+        with open("view_pools_fees.sql", "r") as f:
+            fees_sql = f.read().format(args.pool_address)
+        with open("view_pools_tokens_denorm_weights.sql", "r") as f:
+            denorms_sql = f.read().format(args.pool_address)
 
-    client = bigquery.Client()
-    query_save(client, args.pool_address, "new", new_sql)
-    query_save(client, args.pool_address, "join", join_sql)
-    query_save(client, args.pool_address, "swap", swap_sql)
-    query_save(client, args.pool_address, "exit", exit_sql)
-    query_save(client, args.pool_address, "transfer", transfer_sql)
-    query_save(client, args.pool_address, "fees", fees_sql)
-    query_save(client, args.pool_address, "denorms", denorms_sql)
+        client = bigquery.Client()
+        query_save(client, args.pool_address, "new", new_sql)
+        query_save(client, args.pool_address, "join", join_sql)
+        query_save(client, args.pool_address, "swap", swap_sql)
+        query_save(client, args.pool_address, "exit", exit_sql)
+        query_save(client, args.pool_address, "transfer", transfer_sql)
+        query_save(client, args.pool_address, "fees", fees_sql)
+        query_save(client, args.pool_address, "denorms", denorms_sql)
 
-else:
-    new_results = read_query_results(args.pool_address, "new")
-    join_results = read_query_results(args.pool_address, "join")
-    swap_results = read_query_results(args.pool_address, "swap")
-    exit_results = read_query_results(args.pool_address, "exit")
-    transfer_results = read_query_results(args.pool_address, "transfer")
-    fees_results = read_query_results(args.pool_address, "fees")
-    denorms_results = read_query_results(args.pool_address, "denorms")
+    else:
+        new_results = read_query_results(args.pool_address, "new")
+        join_results = read_query_results(args.pool_address, "join")
+        swap_results = read_query_results(args.pool_address, "swap")
+        exit_results = read_query_results(args.pool_address, "exit")
+        transfer_results = read_query_results(args.pool_address, "transfer")
+        fees_results = read_query_results(args.pool_address, "fees")
+        denorms_results = read_query_results(args.pool_address, "denorms")
 
-    join_results["type"]="join"
-    swap_results["type"]="swap"
-    exit_results["type"]="exit"
-    transfer_results["type"]="transfer"
-    g = join_results.append(swap_results).append(exit_results).append(transfer_results)
-    grouped_by_tx = g.groupby("transaction_hash")
+        join_results["type"]="join"
+        swap_results["type"]="swap"
+        exit_results["type"]="exit"
+        transfer_results["type"]="transfer"
+        g = join_results.append(swap_results).append(exit_results).append(transfer_results)
+        grouped_by_tx = g.groupby("transaction_hash")
 
-    # Pandas, please don't truncate columns when I print them out
-    pd.set_option('display.max_colwidth', None)
+        # Pandas, please don't truncate columns when I print them out
+        pd.set_option('display.max_colwidth', None)
 
-    # actions = []
-    # print("df stuff")
-    # for txhash, events in grouped_by_tx:
-    #     if len(events) > 1:
-    #         events["type"] = events["type"] + "_swap"
+        # actions = []
+        # print("df stuff")
+        # for txhash, events in grouped_by_tx:
+        #     if len(events) > 1:
+        #         events["type"] = events["type"] + "_swap"
 
-    #     e = events.to_dict(orient='index')
-    #     # {272: {'block_number': 11558369,...}, 276: {'block_number':...}}
+        #     e = events.to_dict(orient='index')
+        #     # {272: {'block_number': 11558369,...}, 276: {'block_number':...}}
 
-    #     subactions = [e[i] for i in e.keys()]
-    #     # [{'block_number': 11558...}, {'block_number': ....}]
-    #     actions.append(Action(tx_type=subactions[0]["type"], tx_hash=txhash, datetime=datetime.fromtimestamp(subactions[0]["block_timestamp"]/1000).isoformat(sep=" "), parameters=subactions))
+        #     subactions = [e[i] for i in e.keys()]
+        #     # [{'block_number': 11558...}, {'block_number': ....}]
+        #     actions.append(Action(tx_type=subactions[0]["type"], tx_hash=txhash, datetime=datetime.fromtimestamp(subactions[0]["block_timestamp"]/1000).isoformat(sep=" "), parameters=subactions))
 
-    # actions_dict = [a.to_dict() for a in actions]
+        # actions_dict = [a.to_dict() for a in actions]
 
     # actions_filename = args.pool_address + "-actions.json"
     # print("saving to", actions_filename)
     # with open(actions_filename, 'w') as f:
     #     json.dump(actions_dict, f, indent="\t")
+
+produce_actions()
