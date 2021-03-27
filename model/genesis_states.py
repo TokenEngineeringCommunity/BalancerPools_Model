5ccc52509beb8789624eb66fd4590f7bf385c19f
Andrew Chiw <randomshinichi4869@gmail.com>
2021-03-27 20:06:29 +0100

    fix: pool initial state should really have the right types from the beginning, and not rely on state update functions to coerce the values into the right types

--- model/genesis_states.py
+++ model/genesis_states.py
@@ -12,7 +12,7 @@ from model.models import Token
 
 def generate_initial_state(initial_values_json: str, spot_price_base_currency: str) -> typing.Dict:
     with open(initial_values_json, "r") as f:
-        initial_values = json.load(f, object_hook=token_finding_hook)
+        initial_values = json.load(f, object_hook=convert_to_token_class)
     # Figure out the tokens that are NOT the spot_price_base_currency
     other_tokens = [*initial_values['pool']['tokens'].keys()]
     other_tokens.remove(spot_price_base_currency)
@@ -28,10 +28,11 @@ def generate_initial_state(initial_values_json: str, spot_price_base_currency: s
                                                       token_weight_out=Decimal(other_token.denorm_weight),
                                                       swap_fee=Decimal(initial_values['pool']['swap_fee']))
     initial_values["spot_prices"] = spot_prices
+    initial_values['pool']['pool_shares'] = Decimal(initial_values['pool']['pool_shares'])
     return initial_values
 
 
-def token_finding_hook(k):
+def convert_to_token_class(k):
     if "weight" in k and "denorm_weight" in k and "balance" in k and "bound" in k:
         return Token(weight=k["weight"], denorm_weight=k["denorm_weight"], balance=Decimal(k["balance"]), bound=k["bound"])
     return k
